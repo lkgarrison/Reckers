8d5dea66ac40dcd9b608bace2df8a81917bf37f4
Luke Garrison <lgarriso@nd.edu>
2014-09-27 18:23:52 -0400

    Got the tabs at the bottom to work and have animation. From the orderView screen, you can tap "Checkout" at the bottom and transition to the checkout view. the foundational structure of the ReckersViewController is finished and functioning

--- Reckers/ReckersViewController.m
+++ Reckers/ReckersViewController.m
@@ -12,13 +12,44 @@
 
 @interface ReckersViewController ()
 
-@property (weak, nonatomic) IBOutlet PickupView *pickupView;
 @property (weak, nonatomic) IBOutlet UIView *containerView;
+
 
 @end
 
 @implementation ReckersViewController
+// actions
 
-
+- (IBAction)orderButtonTapped:(UIButton *)sender {
+    [UIView animateWithDuration:.5
+                     animations:^(void){
+                         self.containerView.frame = CGRectMake(0,
+                                                               self.containerView.frame.origin.y,
+                                                               self.containerView.frame.size.width,
+                                                               self.containerView.frame.size.height);
+                     }];
+    
+    
+}
+- (IBAction)checkoutButtonTapped:(UIButton *)sender {
+    [UIView animateWithDuration:.5
+                     animations:^(void){
+                         self.containerView.frame = CGRectMake(-self.view.frame.size.width,
+                                          self.containerView.frame.origin.y,
+                                          self.containerView.frame.size.width,
+                                          self.containerView.frame.size.height);
+                     }];
+}
+- (IBAction)pickupButtonTapped:(UIButton *)sender {
+    [UIView animateWithDuration:.5
+                     animations:^(void){
+                         self.containerView.frame = CGRectMake(-2*self.view.frame.size.width,
+                                                                              self.containerView.frame.origin.y,
+                                                                              self.containerView.frame.size.width,
+                                                                              self.containerView.frame.size.height);
+                     }];
+    
+    
+}
 
 @end
