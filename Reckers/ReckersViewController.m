900b4eb5e8581f9deb040afb7411dd935bce8c89
Luke Garrison <lgarriso@nd.edu>
2014-09-27 21:52:07 -0400

    Custom section headers and custom data in some of the sections. Next: use parse data to populate these headers and cells

--- Reckers/ReckersViewController.m
+++ Reckers/ReckersViewController.m
@@ -16,7 +16,8 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *orderView;
 
-@property (strong, nonatomic) NSArray *titles;
+@property (strong, nonatomic) NSArray *pizzas;
+@property (strong, nonatomic) NSArray *sandwiches;
 
 @property (nonatomic) NSInteger selectedSection;
 @property (weak, nonatomic) IBOutlet UITableView *collapsibleTable;
@@ -31,8 +32,10 @@
     self.orderView.delegate = self;
     self.orderView.dataSource = self;
     
-    self.titles = @[@"Hello", @"How are you"];
     self.selectedSection = -1;
+    
+    self.pizzas = @[@"Pepperoni", @"Cheese"];
+    self.sandwiches = @[@"Chicken", @"Turkey", @"Peanut Butter"];
     
 }
 
@@ -46,7 +49,17 @@
     if (self.selectedSection == -1) {
         return 0;
     } else if (section == self.selectedSection) {
-        return 2;
+        
+        switch (section) {
+            case 0:
+                return self.pizzas.count;
+                
+            case 1:
+                return self.sandwiches.count;
+                
+            default:
+                return 2;
+        }
     } else {
         return 0;
     }
@@ -54,11 +67,40 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"orderCell"];
     
     if (cell) {
         // Configure the cell
-        cell.textLabel.text = self.titles[indexPath.row];
+        
+        switch (indexPath.section) {
+            case 0:
+                cell.textLabel.text = self.pizzas[indexPath.row];
+                break;
+            case 1:
+                cell.textLabel.text = self.sandwiches[indexPath.row];
+                break;
+                // ...
+            case 2:
+                cell.textLabel.text = NSLocalizedString(@"Piadinas", @"Piadinas");
+                break;
+            case 3:
+                cell.textLabel.text = NSLocalizedString(@"Sides", @"Sides");
+                break;
+            case 4:
+                cell.textLabel.text = NSLocalizedString(@"Salads", @"Salads");
+                break;
+            case 5:
+                cell.textLabel.text = NSLocalizedString(@"Breakfast", @"Breakfast");
+                break;
+            default:
+                cell.textLabel.text = @"";
+                break;
+
+        }
+        
+        cell.detailTextLabel.text = @"$2.00";
+        
     }
     
     return cell;
@@ -70,7 +112,40 @@ viewForHeaderInSection:(NSInteger)section {
     UITableViewHeaderFooterView *header = [[UITableViewHeaderFooterView alloc] initWithReuseIdentifier:@"orderHeader"];
     
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 17)];
-    label.text = @"Header";
+    
+    //test
+    NSString *sectionName;
+    switch (section)
+    {
+        case 0:
+            sectionName = NSLocalizedString(@"Pizza", @"Pizza");
+            break;
+        case 1:
+            sectionName = NSLocalizedString(@"Sandwiches", @"Sandwiches");
+            break;
+            // ...
+        case 2:
+            sectionName = NSLocalizedString(@"Piadinas", @"Piadinas");
+            break;
+        case 3:
+            sectionName = NSLocalizedString(@"Sides", @"Sides");
+            break;
+        case 4:
+            sectionName = NSLocalizedString(@"Salads", @"Salads");
+            break;
+        case 5:
+            sectionName = NSLocalizedString(@"Breakfast", @"Breakfast");
+            break;
+        default:
+            sectionName = @"";
+            break;
+    }
+    
+    
+    
+    //@"Pizza"
+    label.text = sectionName;
+    
     
     header.tag = section;
     
